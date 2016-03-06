9b30577ff9499d960f719945916493c60ef7b313
Ilja <ikempf@xebia.fr>
2016-03-06 18:46:57 +0100

    1.1 Averaging Ints - (Implement an algorithm to compute the average of a collection of Ints)

---
+++ src/main/scala/fr/xebia/xke/fp5/exercise1/Average.scala
@@ -0,0 +1,14 @@
+package fr.xebia.xke.fp5.exercise1
+
+/**
+  * Exercise de motivation pour le typeclass pattern. L'idée est d'implementer une fonctionne qui calcule la moyenne d'une liste
+  * d'entiers, en suite d'une liste de double, et en suite d'une liste de numéros complexes
+  */
+object OopAverage {
+
+  /*
+  Implémenter une fonction qui calcule l'average d'une liste d'entiers
+   */
+  def avgInt(numbers: Int*): Int = ???
+
+}
